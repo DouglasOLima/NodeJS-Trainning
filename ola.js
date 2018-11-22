25ee9fe0874293c3131fb4b20043f4179ced726f
NoGemini <idonotgemini@gmail.com>
2018-11-22 10:47:40 -0200

    Inclusão do serviço http

--- ola.js
+++ ola.js
@@ -1,1 +1,11 @@
-console.log('Olá Mundo!');
+var http = require('http');
+
+var server = http.createServer( function(req, res)
+{
+	res.end('<html><body>Olá Mundo</body></html>');
+});
+
+server.listen(3128);
+
+console.log('Executando na porta 3128');
+
